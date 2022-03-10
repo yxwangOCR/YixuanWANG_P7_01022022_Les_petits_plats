294ee11c34cd72ac511b9306e6b179f36584bd59
yxwangOCR <wang.yixuan@live.fr>
2022-03-10 17:46:22 -0500

    responsive css

--- dist/index.5baa4167.js
+++ dist/index.5baa4167.js
@@ -554,7 +554,7 @@ function displayRecipes() {
                                 <div class="recipe-name">${recipe.name}</div>
                                 <div class="time">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512zM232 256C232 264 236 271.5 242.7 275.1L338.7 339.1C349.7 347.3 364.6 344.3 371.1 333.3C379.3 322.3 376.3 307.4 365.3 300L280 243.2V120C280 106.7 269.3 96 255.1 96C242.7 96 231.1 106.7 231.1 120L232 256z"/></svg>
-                                ${recipe.time} min</div>
+                                ${recipe.time} mins</div>
                             </div>
                             <div class="preparation">${ingredientsWrapper.outerHTML}
                               <p class="description">${recipe.description}</p>
@@ -2508,9 +2508,8 @@ parcelHelpers.export(exports, "onSearch", ()=>onSearch
 var _data = require("./data");
 var _dataDefault = parcelHelpers.interopDefault(_data);
 const onSearch = (event)=>{
-    console.log(' ====== OnSearch ========');
-    console.log(event.target.value) // Valeur ecrite pas le user
-    ;
+    console.log(" ====== OnSearch ========");
+    console.log(event.target.value); // Valeur ecrite pas le user
     const searchValue = event.target.value;
     const recipesFiltered = _dataDefault.default.filter((recipe)=>recipe.name.includes(searchValue)
     );
@@ -2519,22 +2518,7 @@ const onSearch = (event)=>{
     //Poisson Cru à la tahitienne
     // Poulet coco réunionnais
     console.log(recipesFiltered);
-} /*
-let upperLetters = [];
-let lowerLetters = [];
-let keyLetter = recipes[0]; // recipe name key work value
-
-function simpleSort(recipes) {
-  if (recipes.length < 3) return recipes;
-  // sort by recipts name:
-  for (let i = 0; i < recipes.length; i++) {
-    if (recipes[i].name > keyLetter.name) {
-      upperLetters.push(recipes[i]);
-    } else lowerLetters.push(recipes[i]);
-  }
-  return upperLetters.concat(keyLetter).concat(lowerLetters);
-}
-*/ ;
+};
 
 },{"./data":"9kapS","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["d5kvp","igcvL"], "igcvL", "parcelRequiredaa1")
 
--- src/sort_simple.js
+++ src/sort_simple.js
@@ -6,40 +6,20 @@ barre de recherche;
 3. La recherche principale affiche les premiers résultats le plus rapidement possible;
 */
 
-
 export const onSearch = (event) => {
-  console.log(' ====== OnSearch ========')
-  console.log(event.target.value) // Valeur ecrite pas le user
-  const searchValue = event.target.value
+  console.log(" ====== OnSearch ========");
+  console.log(event.target.value); // Valeur ecrite pas le user
+  const searchValue = event.target.value;
 
-  const recipesFiltered =  recipes.filter(recipe => recipe.name.includes(searchValue))
-/* TODO En plus du filtre faut rajouter par ingredient et par description  */
-
-
+  const recipesFiltered = recipes.filter((recipe) =>
+    recipe.name.includes(searchValue)
+  );
+  /* TODO En plus du filtre faut rajouter par ingredient et par description  */
 
   // Exemple : coco ou Coco
   // Limonade de Coco
   //Poisson Cru à la tahitienne
   // Poulet coco réunionnais
 
-console.log(recipesFiltered)
-}
-
-
-
-/*
-let upperLetters = [];
-let lowerLetters = [];
-let keyLetter = recipes[0]; // recipe name key work value
-
-function simpleSort(recipes) {
-  if (recipes.length < 3) return recipes;
-  // sort by recipts name:
-  for (let i = 0; i < recipes.length; i++) {
-    if (recipes[i].name > keyLetter.name) {
-      upperLetters.push(recipes[i]);
-    } else lowerLetters.push(recipes[i]);
-  }
-  return upperLetters.concat(keyLetter).concat(lowerLetters);
-}
-*/
+  console.log(recipesFiltered);
+};
